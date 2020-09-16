f55c514a086f6dde6fc2b34370d5bf72d2a6a5d5
Tom Haber <tom.haber@uhasselt.be>
2020-09-16 16:10:12 +0200

    compute_snn

--- neighbours.jl
+++ neighbours.jl
@@ -10,3 +10,13 @@ function ann(X, k; eps=0.0)
 	nn_index, distances
 end
 
+function compute_snn(X, k; eps=0.0, prune=1/15)
+	nn_index, distances = ann(X, k, eps=eps)
+	nn = sparse(repeat(1:size(nnindex,1),20), vec(nnindex), ones(length(nnindex)))
+
+	snn = nn * nn'
+	f(x) = x / (k + (k - x))
+	nonzeros(snn) .= f.(nonzeros(snn))
+	droptol!(snn, prune)
+	snn
+end
