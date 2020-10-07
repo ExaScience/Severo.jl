2719b510b1d092cea2bf956ebb1ffc47946b6cf4
Tom Haber <tom.haber@uhasselt.be>
2020-10-07 19:43:43 +0200

    sce_to_h5 script updated

--- sce_to_h5.R
+++ sce_to_h5.R
@@ -3,14 +3,31 @@ suppressPackageStartupMessages({
 	library(SingleCellExperiment)
 })
 
-source("h5helper.R")
+thisFile <- function() {
+	cmdArgs <- commandArgs(trailingOnly = FALSE)
+	needle <- "--file="
+	match <- grep(needle, cmdArgs)
+	if (length(match) > 0) {
+		# Rscript
+		return(normalizePath(sub(needle, "", cmdArgs[match])))
+	} else {
+		# 'source'd via R console
+		return(normalizePath(sys.frames()[[1]]$ofile))
+	}
+}
+
+source(file.path(dirname(thisFile()), "h5helpers.R"))
 
 iname="input.rds"
-oname="sce.h5"
+oname <- NULL
 
 args <- (commandArgs(trailingOnly = TRUE))
 for (i in 1:length(args)) {
 	eval(parse(text = args[[i]]))
+}
+
+if(is.null(oname)) {
+	oname <- paste0(tools::file_path_sans_ext(iname), ".h5")
 }
 
 sce <- readRDS(iname)
@@ -20,6 +37,6 @@ x <- as(dat, "dgCMatrix")
 cat("percentage nonzeros", length(x@x) / prod(x@Dim), "\n")
 
 invisible(h5createFile(oname))
-h5writeSparse(x, oname, "counts")
+h5writeSparse(x, oname, "counts", counts=T)
 x <- colData(sce)
 h5writeDataFrame(x, oname, "metadata")
