f6b968e278c9282f0de9ec9ab7d03196a6d9733a
Tom Haber <tom.haber@uhasselt.be>
2020-10-25 17:16:10 +0100

    nearest neighbour routines

--- src/Cell.jl
+++ src/Cell.jl
@@ -11,6 +11,8 @@ NamedCountMatrix{T} = NamedArray{T} where {T <: Integer}
 DataMatrix{T} = SparseMatrixCSC{T, Int64} where {T <: Integer}
 NamedDataMatrix{T} = NamedArray{T} where {T <: Integer}
 
+NeighbourGraph{T} = NamedArray{T, 2, SparseMatrixCSC{T, Int64}} where {T <: Real}
+
 try
     include(joinpath(dirname(@__DIR__), "deps","deps.jl"))
 catch e
@@ -20,8 +22,10 @@ end
 include("utils.jl")
 include("input.jl")
 include("normalize.jl")
+include("neighbours.jl")
 
 export CountMatrix, NamedCountMatrix, DataMatrix, NamedDataMatrix
 export read_10X, read_10X_h5, convert_counts, filter_counts
 export normalize
+export nearest_neigbours, shared_nearest_neigbours
 end # module
--- src/neighbours.jl
+++ src/neighbours.jl
@@ -1,24 +1,85 @@
-import SparseArrays: sparse, nonzeros, droptol!
+import SparseArrays: sparse, nonzeros, droptol!, diag
 
 function ann(X, k; ntables=2*size(X,2))
-	n,d = size(X)
+    n,d = size(X)
 
-	nn_index = Matrix{Int32}(undef, n, k)
-	distances = Matrix{Float64}(undef, n, k)
+    nn_index = Matrix{Int32}(undef, n, k)
+    distances = Matrix{Float64}(undef, n, k)
 
-	ccall(("FindNeighbours", "./neighbours.so"), Cvoid,
-		(Ptr{Float64}, Cint, Cint, Cint, Cint, Ptr{Int32}, Ptr{Float64}),
-		X, n, d, k, ntables, nn_index, distances)
-	nn_index, distances
+    ccall(("FindNeighbours", libcell), Cvoid,
+        (Ptr{Float64}, Cint, Cint, Cint, Cint, Ptr{Int32}, Ptr{Float64}),
+        X, n, d, k, ntables, nn_index, distances)
+    nn_index, distances
 end
 
-function compute_snn(X, k; ntables=2*size(X,2), prune=1/15)
-	nn_index, distances = ann(X, k, ntables=ntables)
-	nn = sparse(repeat(1:size(nn_index,1),k), vec(nn_index), ones(length(nn_index)))
+function nearest_neigbours(X::AbstractMatrix, k::Int64; ntables::Int64=2*size(X,2))
+    nn_index, distances = ann(X, k, ntables=ntables)
+    sparse(repeat(1:size(nn_index,1),k), vec(nn_index), trues(length(nn_index)))
+end
 
-	snn = nn * nn'
-	f(x) = x / (k + (k - x))
-	nonzeros(snn) .= f.(nonzeros(snn))
-	droptol!(snn, prune)
-	snn
+function jaccard_index(nn::SparseMatrixCSC, k; prune=1/15)
+    snn = convert(SparseMatrixCSC{Float64}, nn * nn')
+    f(x) = x / (k + (k - x))
+    nonzeros(snn) .= f.(nonzeros(snn))
+    droptol!(snn, prune)
+    snn
+end
+
+function jaccard_index(nn::SparseMatrixCSC; prune=1/15)
+    snn = convert(SparseMatrixCSC{Float64}, nn * nn')
+    k = diag(snn)
+
+    nz = nonzeros(snn)
+    @inbounds for i in 1:size(snn,2)
+        x = view(nz, nzrange(snn, i))
+        x ./= (k[i] .+ (k[i] .- x))
+    end
+
+    droptol!(snn, prune)
+    snn
+end
+
+"""
+    nearest_neigbours(X::NamedArray{T,2}, k::Int64; ntables::Int64=2*size(X,2)) where T
+
+Compute a k-nearest neighbours graph based on coordinates for each cell.
+
+**Arguments**:
+
+    - `X`: a labelled matrix with coordinates for each cell
+    - `k`: number of nearest neighbours to find
+    - `ntables`: number of tables to use in knn algorithm: controls the precision (higher is more accurate)
+
+**Return values**:
+
+A k-nearest neighbours graph represented by a sparse matrix
+"""
+function nearest_neigbours(X::NamedArray{T,2}, k::Int64; ntables::Int64=2*size(X,2)) where T
+    nn = nearest_neigbours(X.array, k; ntables=ntables)
+    NamedArray(nn, (X.dicts[1], X.dicts[1]), (:cells, :cells))
+end
+
+"""
+    shared_nearest_neigbours(X::NamedArray{T,2}, k::Int64; ntables::Int64=2*size(X,2)) where T
+
+Compute a k-nearest neighbours graph based on coordinates for each cell and its Jaccard index.\\
+The Jaccard index measures similarity between nearest neighbour sets, and is defined as
+the size of the intersection divided by the size of the union. "0" indicating no overlap and "1" indicating full overlap.
+
+**Arguments**:
+
+    - `X`: a labelled matrix with coordinates for each cell
+    - `k`: number of nearest neighbours to find
+    - `ntables`: number of tables to use in knn algorithm: controls the precision (higher is more accurate)
+    - `prune`: cutoff for the Jaccard index, edges with values below this cutoff are removed from the resulting graph
+
+**Return values**:
+
+A shared nearest neighbours graph represented by a sparse matrix. Weights of the edges indicate similarity of
+the neighbourhoods of the cells as computed with the Jaccard index.
+"""
+function shared_nearest_neigbours(X::NamedArray{T,2}, k::Int64; ntables::Int64=2*size(X,2), prune=1/15) where T
+    nn = nearest_neigbours(X.array, k; ntables=ntables)
+    snn = jaccard_index(nn, k; prune=prune)
+    NamedArray(snn, (X.dicts[1], X.dicts[1]), (:cells, :cells))
 end
