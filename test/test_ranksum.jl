da90554708f351f4a6d8c7788fc7d507cb78feb8
Tom Haber <tom.haber@uhasselt.be>
2020-11-27 13:03:01 +0100

    updated tests

--- test/test_ranksum.jl
+++ test/test_ranksum.jl
@@ -3,6 +3,7 @@ using Cell, Test
 @testset "ranksum" begin
     statistics = [1.83,  0.50,  1.62,  2.48, 1.68, 1.88, 1.55, 3.06, 1.30, 0.878, 0.647, 0.598, 2.05, 1.06, 1.29, 1.06, 3.14, 1.29]
     labels = BitArray([1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-    pval = 0.13291945818531886
-    @test Cell.ranksumtest(statistics, labels) ≈ pval
+    true_pval = 0.13291945818531886
+    z, pval = Cell.ranksumtest(statistics, labels)
+    @test pval ≈ true_pval
 end
