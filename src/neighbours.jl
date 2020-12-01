02ec112be1aeaeb3fc6bac345e62e8952da167b6
Tom Haber <tom.haber@uhasselt.be>
2020-12-01 18:10:08 +0100

    export jaccard_index and additional docs + dims arguments

--- src/neighbours.jl
+++ src/neighbours.jl
@@ -17,7 +17,7 @@ function nearest_neigbours(X::AbstractMatrix, k::Int64; ntables::Int64=2*size(X,
     sparse(repeat(1:size(nn_index,1),k), vec(nn_index), trues(length(nn_index)))
 end
 
-function jaccard_index(nn::SparseMatrixCSC, k; prune=1/15)
+function jaccard_index(nn::SparseMatrixCSC, k; prune::Real=1/15)
     snn = convert(SparseMatrixCSC{Float64}, nn * nn')
     f(x) = x / (k + (k - x))
     nonzeros(snn) .= f.(nonzeros(snn))
@@ -25,7 +25,7 @@ function jaccard_index(nn::SparseMatrixCSC, k; prune=1/15)
     snn
 end
 
-function jaccard_index(nn::SparseMatrixCSC; prune=1/15)
+function jaccard_index(nn::SparseMatrixCSC; prune::Real=1/15)
     snn = convert(SparseMatrixCSC{Float64}, nn * nn')
     k = diag(snn)
 
@@ -40,6 +40,24 @@ function jaccard_index(nn::SparseMatrixCSC; prune=1/15)
 end
 
 """
+    jaccard_index(X::NamedArray{T,2}; prune::Real=1/15) where T
+
+Compute a graph with edges defined by the jaccard index. The Jaccard index measures similarity
+between nearest neighbour sets, and is defined as the size of the intersection divided by the size
+of the union. "0" indicating no overlap and "1" indicating full overlap.
+
+**Arguments**:
+    - `nn`: a nearest neighbour graph
+    - `prune`: cutoff for the Jaccard index, edges with values below this cutoff are removed from the resulting graph
+
+**Return values**:
+
+A shared nearest neighbours graph represented by a sparse matrix. Weights of the edges indicate similarity of
+the neighbourhoods of the cells as computed with the Jaccard index.
+"""
+jaccard_index(nn::NamedArray{T,2}; prune::Real=1/15) where T = jaccard_index(nn.array; prune=prune)
+
+"""
     nearest_neigbours(X::NamedArray{T,2}, k::Int64; ntables::Int64=2*size(X,2)) where T
 
 Compute a k-nearest neighbours graph based on coordinates for each cell.
@@ -48,18 +66,40 @@ Compute a k-nearest neighbours graph based on coordinates for each cell.
 
     - `X`: a labelled matrix with coordinates for each cell
     - `k`: number of nearest neighbours to find
+    - `dims`: which dimensions to use
     - `ntables`: number of tables to use in knn algorithm: controls the precision (higher is more accurate)
 
 **Return values**:
 
 A k-nearest neighbours graph represented by a sparse matrix
 """
-function nearest_neigbours(X::NamedArray{T,2}, k::Int64; ntables::Int64=2*size(X,2)) where T
+function nearest_neigbours(X::NamedArray{T,2}, k::Int64, dims; ntables::Int64=2*size(X,2)) where T
+    nn = nearest_neigbours(X.array[:,dims], k; ntables=ntables)
+    NamedArray(nn, (X.dicts[1], X.dicts[1]), (:cells, :cells))
+end
+
+function nearest_neigbours(X::NamedArray{T,2}, k::Int64, ::Colon=:; ntables::Int64=2*size(X,2)) where T
     nn = nearest_neigbours(X.array, k; ntables=ntables)
     NamedArray(nn, (X.dicts[1], X.dicts[1]), (:cells, :cells))
 end
 
-nearest_neigbours(em::LinearEmbedding, k::Int64; kw...) = nearest_neigbours(em.coordinates, k; kw...)
+"""
+    nearest_neigbours(em::LinearEmbedding, k::Int64; ntables::Int64=2*size(X,2)) where T
+
+Compute a k-nearest neighbours graph based on an embedding
+
+**Arguments**:
+
+    - `em`: embedding containing the transformed coordinates for each cell
+    - `k`: number of nearest neighbours to find
+    - `dims`: which dimensions to use
+    - `ntables`: number of tables to use in knn algorithm: controls the precision (higher is more accurate)
+
+**Return values**:
+
+A k-nearest neighbours graph represented by a sparse matrix
+"""
+nearest_neigbours(em::LinearEmbedding, k::Int64, dims=:; kw...) = nearest_neigbours(em.coordinates, k, dims; kw...)
 
 """
     shared_nearest_neigbours(X::NamedArray{T,2}, k::Int64; ntables::Int64=2*size(X,2)) where T
@@ -72,6 +112,7 @@ the size of the intersection divided by the size of the union. "0" indicating no
 
     - `X`: a labelled matrix with coordinates for each cell
     - `k`: number of nearest neighbours to find
+    - `dims`: which dimensions to use
     - `ntables`: number of tables to use in knn algorithm: controls the precision (higher is more accurate)
     - `prune`: cutoff for the Jaccard index, edges with values below this cutoff are removed from the resulting graph
 
@@ -80,7 +121,13 @@ the size of the intersection divided by the size of the union. "0" indicating no
 A shared nearest neighbours graph represented by a sparse matrix. Weights of the edges indicate similarity of
 the neighbourhoods of the cells as computed with the Jaccard index.
 """
-function shared_nearest_neigbours(X::NamedArray{T,2}, k::Int64; ntables::Int64=2*size(X,2), prune=1/15) where T
+function shared_nearest_neigbours(X::NamedArray{T,2}, k::Int64, dims; ntables::Int64=2*size(X,2), prune=1/15) where T
+    nn = nearest_neigbours(X.array[:,dims], k; ntables=ntables)
+    snn = jaccard_index(nn, k; prune=prune)
+    NamedArray(snn, (X.dicts[1], X.dicts[1]), (:cells, :cells))
+end
+
+function shared_nearest_neigbours(X::NamedArray{T,2}, k::Int64, ::Colon=:; ntables::Int64=2*size(X,2), prune=1/15) where T
     nn = nearest_neigbours(X.array, k; ntables=ntables)
     snn = jaccard_index(nn, k; prune=prune)
     NamedArray(snn, (X.dicts[1], X.dicts[1]), (:cells, :cells))
