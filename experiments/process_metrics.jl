068d2c1788e55ebac674ae8b1b90edd55f8828a7
Tom Haber <tom.haber@uhasselt.be>
2021-09-01 11:36:15 +0200

    processing of output

---
+++ experiments/process_metrics.jl
@@ -0,0 +1,41 @@
+using HDF5
+using CSV
+using DataFrames
+import Clustering: randindex
+
+jaccard(A, B) = (A = Set(A); B = Set(B); length(intersect(A,B)) / length(union(A,B)))
+
+df = DataFrame()
+for fname in filter(endswith("h5"), readdir())
+    try
+        name, _ = splitext(fname)
+        it, size = if name == "fullsample"
+            1, 1_000_000
+        else
+            parts = split(name, "_")
+            it = parse(Int64, parts[3])
+            size = parse(Int64, parts[5])
+            it, size
+        end
+
+        h5open(fname, "r") do io
+            if haskey(io, "R_lbls")
+                lbls_ref = read(io, "R_lbls")
+                hvf_ref = read(io, "R_hvf")
+
+                for prefix in filter(x->haskey(io, "$(x)_t"), ["jl", "jl32", "py"])
+                    lbls = read(io, "$(prefix)_lbls")
+                    hvf = read(io, "$(prefix)_hvf")
+                    ari, ri, _ = randindex(lbls_ref, lbls)
+                    j = jaccard(hvf_ref, hvf)
+                    push!(df, (dataset=name, it=it, size=size, implementation=prefix, ari=ari, ri=ri, jaccard=j))
+                end
+
+            end
+            df
+        end
+    catch e
+        println(stderr, "Failed to process $fname")
+    end
+end
+CSV.write(stdout, df)
--- experiments/process_subsamples.jl
+++ experiments/process_subsamples.jl
@@ -15,22 +15,29 @@ df = DataFrame()
 for fname in readdir()
     try
         name, _ = splitext(fname)
-        parts = split(name, "_")
-        it = parse(Int64, parts[3])
-        size = parse(Int64, parts[5])
+        it, size = if name == "fullsample"
+            1, 1_000_000
+        else
+            parts = split(name, "_")
+            it = parse(Int64, parts[3])
+            size = parse(Int64, parts[5])
+            it, size
+        end
 
         h5open(fname, "r") do io
-            for (prefix, steps) in [("R", steps_R), ("jl", steps_jl), ("py", steps_R)]
+            for (prefix, steps) in [("R", steps_R), ("jl", steps_jl), ("jl32", steps_jl), ("py", steps_R)]
                 if haskey(io, "$(prefix)_t")
+                    lbls = read(io, "$(prefix)_lbls")
                     t = read_time(io, prefix)
-                    vals = [getindex(t, k) for k in steps]
-                    append!(df, DataFrame(dataset=name, it=it, size=size, implementation=prefix, step=steps_R, t=vals))
+                    vals = [get(t, k, missing) for k in steps]
+                    append!(df, DataFrame(dataset=name, it=it, size=size, implementation=prefix, step=steps_R, t=vals, clusters=length(unique(lbls))))
                 end
             end
             df
         end
-    catch
-        println("Failed to process $fname")
+    catch e
+        println(stderr, "Failed to process $fname")
+        #showerror(stderr, e)
     end
 end
 CSV.write(stdout, df)
