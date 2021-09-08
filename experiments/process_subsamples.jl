311949277a5d01e0abb00e8347ba877e245bd5ad
Tom Haber <tom.haber@uhasselt.be>
2021-09-08 15:07:01 +0200

    generalize post-processing script beyond subsample datasets

--- experiments/process_subsamples.jl
+++ experiments/process_subsamples.jl
@@ -12,27 +12,39 @@ function read_time(ds)
     Dict(zip(k,v))
 end
 
+function find_cellcount(f)
+    for ds in f
+        if haskey(ds, "lbls")
+            return length(ds["lbls"])
+        end
+    end
+
+    error("cell count not found")
+end
+
 df = DataFrame()
 for fname in filter(endswith(".h5"), readdir())
     try
         name, _ = splitext(fname)
-        it, size = if name == "fullsample"
-            1, 1_000_000
-        else
-            parts = split(name, "_")
-            it = parse(Int64, parts[3])
-            size = parse(Int64, parts[5])
-            it, size
-        end
-
         h5open(fname, "r") do io
+            it = if startswith(name, "subsample")
+                parts = split(name, "_")
+                it = parse(Int64, parts[3])
+                size = parse(Int64, parts[5])
+                it
+            else
+                1
+            end
+
+            size = find_cellcount(io)
+
             for (prefix, steps) in [("R", steps_R), ("jl", steps_jl), ("jl32", steps_jl), ("py", steps_R), ("jl32_opt", steps_jl), ("jl_opt", steps_jl)]
                 if haskey(io, prefix)
                     ds = io[prefix]
                     lbls = read(ds, "lbls")
                     t = read_time(ds)
                     vals = [get(t, k, missing) for k in steps]
-                    append!(df, DataFrame(dataset=name, it=it, size=size, implementation=prefix, step=steps_R, t=vals, clusters=length(unique(lbls))))
+                    append!(df, DataFrame(dataset=name, it=it, size=size, implementation=prefix, step=steps_R, time=vals, clusters=length(unique(lbls))))
                 end
             end
             df
