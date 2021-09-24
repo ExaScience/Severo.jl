1df834223a64ecf0a4ea477212f767067bc96fbc
Tom Haber <tom.haber@uhasselt.be>
2021-09-24 11:31:26 +0200

    AGPL license

--- test/test_diffexr.jl
+++ test/test_diffexr.jl
@@ -1,4 +1,16 @@
-# copyright imec - evaluation license - not for distribution
+# Severo: a software package for analysis and exploration of single-cell RNA-seq datasets.
+# Copyright (c) 2021 imec vzw.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version, and Additional Terms
+# (see below).
+
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
 
 using Test
 using Severo
